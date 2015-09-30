eea9a6e8920d57c2ea8d9bdddde30e0f703fa587
battosai30 <batto@hotmail.fr>
2015-09-30 16:26:57 +0200

    Update

--- Remote_ESP8266.ino
+++ Remote_ESP8266.ino
@@ -3,101 +3,57 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-//char* ssid = "freebox_Avril";
-//char* password = "0651113168";
-
 char* ssid = "xxxxx"; // votre SSID
 char* password = "yyyyyyy"; // votre mot de passe wifi
 long port = 504;
 
-MDNSResponder mdns;
+MDNSResponder mdns; // serveur mDNS
 
-ESP8266WebServer server(port);
+ESP8266WebServer server(port); // serveur HTTP
 
-const int led = 16;
-const int relay =5;
-
-void handle_app() {
-  server.send(200, "text/plain", "opening");
-open();
-}
-
-void handle_root() {
-
-  char page[] = "<h1>Ouverture portail</h1><p><a href=\"open\"><button>Ouvrir</button></a></p>";
-
-  server.send(200, "text/html", page);
-  open();
-
-}
-
-void open() {
-  digitalWrite(relay, 1);
-  Serial.println("Opening");
-  delay(1000);
-  digitalWrite(relay, 0);
-}
-
+const int led = 16; // led integree au NodeMCU, attention logique inverse
+const int relay = 5; // relais connecte au GPIO5
 
 void setup(void) {
-  
-pinMode(relay, OUTPUT);
+
+/* Configuration des entree/sortie */
+  pinMode(relay, OUTPUT);
   pinMode(led, OUTPUT);
-  
+
   digitalWrite(led, 1);
   digitalWrite(relay, 0);
+
+  Serial.begin(115200); // initialisation du port serie
   
-  Serial.begin(115200);
-  connect(ssid, password);
+  connect(ssid, password); // connexion au reseau Wifi
+  
   Serial.println("");
 
+/* demarrage du serveur mDNS sur esp8266.local */
   if (mdns.begin("esp8266", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
 
-  server.on("/open", handle_app);
-
-  server.on("/", handle_root);
-
-  server.begin();
+/* ajout des actions du serveur */
+  server.on("/", handle_root); 
+  server.on("/open", handle_open); 
+ 
+  server.begin(); // demarrage du serveur
+  
   Serial.println("HTTP server started");
 }
 
 void loop(void) {
-  server.handleClient();
-  if (Serial.available()) handleSerial();
-  if (WiFi.status() == WL_CONNECTED) digitalWrite(led,0);
-  else digitalWrite(led,1);
+  
+  server.handleClient(); // gestion du serveur
+
+/* Si connecté au wifi alors la LED s'allume */
+  if (WiFi.status() == WL_CONNECTED) digitalWrite(led, 0);
+  else digitalWrite(led, 1);
+  
 }
 
-void handleSerial() {
-
-  delay(20);
-
-  char buf[200];
-  memset(buf, NULL, sizeof(buf));
-
-  int i = 0;
-
-  while (Serial.available()) buf[i++] = Serial.read();
-
-  char *p;
-
-  p = strtok(buf, ",");
-
-  if (strstr(p, "$") != NULL) {
-Serial.println("");
-    Serial.println("New AP config received");
-
-    char *_ssid = strtok(NULL, ",");
-    char *_pwd = strtok(NULL, ",");
-
-    connect(_ssid, _pwd);
-
-  }
-
-}
-
+/* Routine pour se connecter à un reseau wifi */
 void connect(char *_SSID, char* _PWD) {
 
   Serial.println("");
@@ -106,30 +62,53 @@ void connect(char *_SSID, char* _PWD) {
   Serial.print(" password = ");
   Serial.print( _PWD);
 
-  //WiFi.disconnect();
-  //delay(1500);
   WiFi.begin(_SSID, _PWD);
   Serial.println("");
 
-int h=0;
+  int h = 0;
+  
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    if(h++>40) {
+    
+    if (h++ > 40) { // si trop long on abandonne
       Serial.println();
       Serial.println("Failed to connect");
-      WiFi.begin(ssid, password);
       return;
     }
-    }
-  
-  
+    
+  }
+
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+}
+
+/* Action serveur sur /open */
+void handle_open() {
+  server.send(200, "text/plain", "opening"); // repond "opening"
+  open(); // actionne le relais
+}
+
+void handle_root() {
+
+/* page[] contient notre page web et renvois vers le domaine /open si on appuie sur le bouton */
+  char page[] = "<h1>Ouverture portail</h1><p><a href=\"open\"><button>Ouvrir</button></a></p>";
+
+  server.send(200, "text/html", page); // repond avec la page web codee en HTML
+
+}
+
+void open() {
+  
+  digitalWrite(relay, 1);
+  Serial.println("Opening");
+  delay(1000);
+  digitalWrite(relay, 0);
+  
 }
 
